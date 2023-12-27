2b036f5e7f54642c15d9362b959d8f396b0fcecc
Axel Denouly <axeljoo2935@gmail.com>
2023-12-27 21:52:32 +0100

    geo-app source code

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import ListCard from "./components/ListCard"
+
+// https://restcountries.com/v3.1/region/europe
+
+function App() {
+
+  const [countries, setCountries] = useState()
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/region/europe")
+    .then(data => data.json())
+    .then(data => {
+      data.sort((a,b) => {
+        if (a.name.common < b.name.common) {
+          return -1
+        } 
+        else if (a.name.common > b.name.common) {
+          return 1
+        } 
+        else {
+          return 0
+        }
+      })
+
+      setCountries(data)
+
+    })
+  }, [])
+
+  return (
+    <div className="min-h-screen bg-slate-800">
+      <div className="max-w-7xl mx-auto py-20 px-4">
+        <h1 className="text-gray-50 text-4xl mb-3">Europe Countries Data <br></br><span className=" text-xs text-slate-600">&copy; 2024 Axel Denouly | All Right Reserved </span></h1>
+        <p className="text-gray-100 text-xl mb-8">Click on a card to display the country's informations.</p>
+        {countries && (
+          <ul className=" grid min-[450px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
+            {countries.map((country, index) => (
+              <ListCard key={index} country={country}/>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
